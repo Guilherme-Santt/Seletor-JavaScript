ed33cf7d0f908e2ca4b46ce272e5c7a45cc78a8c
Guilherme-Santt <guisant@icloud.com>
2024-02-15 18:00:47 -0300

    Correção

--- src/script.js
+++ src/script.js
@@ -1,10 +1,6 @@
 const ano = new Date().getFullYear();
 console.log(ano);
 
-const dia = new Date().getDay();
-
-
-
 for (let y = ano; y >= 1900; y-- ) {
     document.getElementById("ano").innerHTML += "<option value='" + y+ "'>" + y + "</option>";
 }
@@ -14,8 +10,6 @@ for (let m = 1; m <= 12; m++ ) {
     document.getElementById("mes").innerHTML += "<option value='" + m + "'>" + m + "</option>";
 }
 
-
-const mes = new Date().getMonth();
 
 for (let d = 1; d <= 31; d++ ) {
     document.getElementById("dia").innerHTML += "<option value='" + d + "'>" + d + "</option>";
